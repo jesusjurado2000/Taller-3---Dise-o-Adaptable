0e662082301db2f07739a4d771a7d3e390656944
jesusjurado2000 <80048009+jesusjurado2000@users.noreply.github.com>
2024-04-24 12:32:52 -0500

    Update compras.js

--- compras.js
+++ compras.js
@@ -1,1 +1,5 @@
-
+const products = [
+  { id: 1, name: "Toalla", price: 10, image: "https://icons.iconarchive.com/icons/hybridworks/yoritsuki/128/Oshibori-wet-hand-towel-icon.png" },
+  { id: 2, name: "Jabon", price: 5, image: "https://icons.iconarchive.com/icons/robinweatherall/cleaning/128/bottles-icon.png" },
+  { id: 3, name: "Cepillo", price: 3, image: "https://icons.iconarchive.com/icons/microsoft/fluentui-emoji-flat/128/Toothbrush-Flat-icon.png" }
+];
